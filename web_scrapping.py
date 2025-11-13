84a00481a70446fad37aebdaef78c33f1a2f3dc9
Dafnis Krasnisi <42390575+dafniskra@users.noreply.github.com>
2025-11-13 16:32:23 +0100

    Update web_scrapping.py

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 13 16:21:31 2025
+
+@author: dkrasniqi
+"""
+
 #https://urlebird.com/fr/hash/mugler/
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -10,3 +18,50 @@ import pandas as pd
 from datetime import datetime
 import os 
 from selenium.webdriver.chrome.service import Service
+import requests
+
+#%%
+page = requests.get("https://www.etreproprio.com/annonces")
+soup = BeautifulSoup(page.content, "html.parser")
+
+#%%
+#%%
+temp = soup.find("div", class_='ep-search-list-wrapper').find_all("a")
+
+#%%
+liens = []
+for i in range(len(temp)):
+    print()
+    if "https://www.etreproprio.com/immobilier-" in temp[i]['href']:
+        liens.append(temp[i]['href'])
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
